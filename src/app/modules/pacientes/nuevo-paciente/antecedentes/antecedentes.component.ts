847c867c989647744ce2cb7d05e22b1034b6f62a
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2023-03-06 17:46:02 -0300

    refactor inputs antecedentes sin terminar

--- src/app/modules/pacientes/nuevo-paciente/antecedentes/antecedentes.component.ts
+++ src/app/modules/pacientes/nuevo-paciente/antecedentes/antecedentes.component.ts
@@ -79,5 +79,60 @@ export class AntecedentesComponent implements OnInit {
     }
     this._servicePacienteNuevo.CargarAntecedentes(this.antecedentes);
   }
+  
+  asignarValor(valor:string,campo:number){      
+    if(valor !== "" ){
+      this.MapAntecedente(valor,campo);  
+    }
+    if(!this.antecedente.ortodoncia)this.antecedente.edadOrtodoncia = undefined; 
+    if(!this.antecedente.embarazos){
+      this.antecedente.abortosEspontaneo = undefined; 
+      this.antecedente.abortosInducido = undefined; 
+      this.antecedente.partos = undefined; 
+    }
+    console.log(this.antecedentes);
+    this._servicePacienteNuevo.CargarAntecedentes(this.antecedentes);
+    
+  }
+
+  MapAntecedente(valor:any,campo:number){
+    switch (campo) {
+      case 1:this.antecedentes.cirugias = valor;break;
+      case 2:this.antecedentes.implanteSuperior = valor;break;
+      case 3:this.antecedentes.implanteInferior = valor;break;
+      case 4:this.antecedentes.piezasFaltantesSup = valor;break;
+      case 5:this.antecedentes.piezasFaltantesInf = valor;break;
+      case 6:this.antecedentes.protesis = valor;break;
+      case 7:this.antecedentes.contencion = valor;break;
+      case 8:this.antecedentes.placaDescanso = valor;break;
+      case 9:this.antecedentes.ortodoncia = valor;break;
+      case 10:this.antecedentes.edadOrtodoncia = valor;break;
+      case 11:this.antecedentes.menstruacion = valor;break;
+      case 12:this.antecedentes.frecuencia = valor;break;
+      case 13:this.antecedentes.duracion = valor;break;
+      case 14:this.antecedentes.volumen = valor;break;
+      case 15:this.antecedentes.embarazos = valor;break;
+      case 16:this.antecedentes.abortosInducido = valor;break;
+      case 17:this.antecedentes.abortosEspontaneo = valor;break;
+      case 18:this.antecedentes.partos = valor;break;
+      case 19:this.antecedentes.intestinal = valor;break;
+      case 20:this.antecedentes.digestivo = valor;break;
+      case 21:this.antecedentes.cardiaco = valor;break;
+      case 22:this.antecedentes.urogenital = valor;break;
+      case 23:this.antecedentes.respiratorio = valor;break;
+      case 24:this.antecedentes.oseo = valor;break;
+      case 25:this.antecedentes.fuma = valor;break;
+      case 26:this.antecedentes.otrasDrogas = valor;break;
+      case 27:this.antecedentes.accidentes = valor;break;
+      case 28:this.antecedentes.diabetes = valor;break;
+      case 29:this.antecedentes.medicacion = valor;break;
+      case 30:this.antecedentes.fracturas = valor;break;
+      case 31:this.antecedentes.dolorCabeza = valor;break;
+      case 32:this.antecedentes.tiroides = valor;break;
+      case 33:this.antecedentes.otros = valor;break;
+      case 34:this.antecedentes.alimentacion = valor;break;
+      case 35:this.antecedentes.perdidas = valor;break;
+    }
+  }
 
 }
